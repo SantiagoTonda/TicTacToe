ff4acad9fda109a4371fba100c93eb229a6761f6
SantiagoTonda <superorbe@gmail.com>
2023-03-25 03:09:54 -0300

    feat: about

--- script.js
+++ script.js
@@ -76,6 +76,7 @@ function emptySquares() {
    Hacer función para cambiar quién empieza
    Hacer función para que la IA no siga jugando si gané
    Cambiar color de los distintos players
+   Cambiar fuente
    La clase cell es necesaria?
    Solucionar error en consola cuando gano en dificultad media?
 
